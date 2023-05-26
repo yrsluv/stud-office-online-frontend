073d8bd0dd6607d70872e16dfb4854e1dab17087
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-05-26 17:19:39 +0300

    archetecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import MainProcess from './processes/MainProcess';
+import LogInProcess from './processes/LogInProcess';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="*" element={<MainProcess />} />
+        <Route path="login" element={<LogInProcess />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './normalize.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import './normalize.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
