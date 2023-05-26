2fe41a3ad64ba57d19b3548c3de13b10344a24cf
yrslv <yrslvvvv@gmail.com>
2023-05-26 19:47:49 +0300

    footer

--- src/processes/MainProcess.tsx
+++ src/processes/MainProcess.tsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
-<<<<<<< HEAD
-=======
 import Footer from '../components/Footer';
->>>>>>> 0b88540a54d109aebf575129e806c258d72b2556
 
 const MainProcess = () => {
   return (
