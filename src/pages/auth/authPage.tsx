d8c0b5f8f4fef7d6afa8eb98e513b4d07466e6c2
yrslv <yrslvvvv@gmail.com>
2023-05-26 19:45:23 +0300

    signin

--- src/pages/auth/authPage.tsx
+++ src/pages/auth/authPage.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './styles.scss';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Button, Input, Space } from 'antd';
+import { Button, Input } from 'antd';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 export const AuthPage = () => {
   const [response, setResponse] = useState<boolean | null>(true);
@@ -12,37 +13,43 @@ export const AuthPage = () => {
 
   return (
     <>
-      <form className="authModal__container">
-        <div className="authModal__fields">
-          <div className="authModal__field">
-            <span className="authModal__label">E-mail </span>
-            <Input placeholder="Введите ваш e-mail" status={response ? undefined : 'error'} />
-          </div>
-          <div className="authModal__field">
-            <span className="authModal__label">Password</span>
-            <Input.Password
-              placeholder="Введите ваш пароль"
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-              status={response ? undefined : 'error'}
-            />
-          </div>
+      <div className="authPage">
+        <div className="authLogos">
+          <Logo />
+          <span className="h3">Личный кабинет студента</span>
         </div>
+        <form className="authModal__container">
+          <div className="authModal__fields colorGray">
+            <div className="authModal__field">
+              <span className="authModal__label longRegular">E-mail </span>
+              <Input placeholder="Введите ваш e-mail" status={response ? undefined : 'error'} />
+            </div>
+            <div className="authModal__field">
+              <span className="authModal__label longRegular">Password</span>
+              <Input.Password
+                placeholder="Введите ваш пароль"
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                status={response ? undefined : 'error'}
+              />
+            </div>
+          </div>
 
-        <div className="authModal__buttonContainer">
-          <Button
-            className="authModal__button"
-            type="primary"
-            onClick={() => {
-              setResponse(false);
-            }}
-          >
-            Войти
-          </Button>
-          {!response && (
-            <span className="authModal__errorAlert colorRed">Неверный логин или пароль</span>
-          )}
-        </div>
-      </form>
+          <div className="authModal__buttonContainer">
+            <Button
+              className="authModal__button longRegular"
+              type="primary"
+              onClick={() => {
+                setResponse(false);
+              }}
+            >
+              Войти
+            </Button>
+            <span className={`authModal__errorAlert colorRed p-small ${!response && 'active'}`}>
+              Неверный логин или пароль
+            </span>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
