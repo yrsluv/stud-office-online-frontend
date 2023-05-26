940a342fe4de255bf94f498930a9ed4b9dda95e6
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-05-27 02:02:27 +0300

    study certificate

--- src/pages/main/StudyCertPage.tsx
+++ src/pages/main/StudyCertPage.tsx
@@ -1,11 +1,52 @@
+import { useState, createRef } from 'react';
+import axios from 'axios';
 import { Button } from 'antd';
+import jsPDF from 'jspdf';
+import '../../assets/times-normal';
+import '../../assets/times-bold';
+import '../../assets/times-italic';
+import '../../assets/times-bolditalic';
 
 const StudyCertPage = () => {
+  const [permissionError, setPermissionError] = useState<boolean>(false);
+  const [pdfGenError, setPdfGenError] = useState<boolean>(false);
+
+  const handleGetSudyCert = async () => {
+    // TODO: get user data
+    // set error
+    try {
+      const htmlResponse = await axios.get('/study-cert.html');
+      console.log(htmlResponse.data);
+
+      const doc = new jsPDF();
+      doc.setLanguage('ru');
+      doc.html(htmlResponse.data, {
+        callback: (doc) => {
+          doc.setLanguage('ru');
+          doc.setFont('times', 'normal');
+          doc.output('dataurlnewwindow');
+        },
+        windowWidth: 600,
+        width: 180,
+        x: 15,
+        y: 10,
+      });
+
+      setPdfGenError(false);
+    } catch {
+      setPdfGenError(true);
+    }
+  };
+
   return (
     <main className="studyCertPage" role="main">
-      <h1 className="h1">Запрос справки об обучении</h1>
+      <h1 className="h1 colorBlue">Запрос справки об обучении</h1>
+      <Button type="primary" onClick={handleGetSudyCert}>
+        Запросить справку об обучении
+      </Button>
 
-      <Button type="primary">Запросить справку об обучении</Button>
+      {permissionError && <p className="colorRed p-small">Вы ещё/уже не учитесь в МИФИ</p>}
+      {pdfGenError && <p className="colorRed p-small">Ошибка генерации справки</p>}
     </main>
   );
 };
