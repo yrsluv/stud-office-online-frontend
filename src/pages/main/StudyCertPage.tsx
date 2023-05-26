6e9b4a57d8e91b10e819aa6885b698251ec173b7
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-05-26 20:08:31 +0300

    study-cert

---
+++ src/pages/main/StudyCertPage.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+const StudyCertPage = () => {
+  return (
+    <main className="studyCertPage" role="main">
+      <h1 className="h1">Запрос справки об обучении</h1>
+
+      <Button type="primary">Запросить справку об обучении</Button>
+    </main>
+  );
+};
+export default StudyCertPage;
--- src/processes/MainProcess.tsx
+++ src/processes/MainProcess.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import StudyCert from '../pages/main/StudyCertPage';
 
 const MainProcess = () => {
   return (
@@ -8,6 +9,9 @@ const MainProcess = () => {
       <Header />
       <Routes>
         <Route path="*" element={<>PageNotFound</>} />
+        <Route path="service">
+          <Route path="study-cert" element={<StudyCert />} />
+        </Route>
       </Routes>
       <Footer />
     </>
